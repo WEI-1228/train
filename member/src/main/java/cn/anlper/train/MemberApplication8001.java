57411b7c4935a32b9a288ee7518b741bb243128d
解矣 <516363082@qq.com>
2024-05-12 13:25:22 +0800

    修改启动日志

--- member/src/main/java/cn/anlper/train/MemberApplication8001.java
+++ member/src/main/java/cn/anlper/train/MemberApplication8001.java
@@ -1,11 +1,17 @@
 package cn.anlper.train;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@Slf4j
 public class MemberApplication8001 {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication8001.class);
+        ConfigurableApplicationContext context = SpringApplication.run(MemberApplication8001.class);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        log.info("启动成功，访问地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 }
