00d6ca54aafac241212e0e79b6a8d2c2b334e85c
解矣 <516363082@qq.com>
2024-05-12 12:05:34 +0800

    新增member会员模块

---
+++ member/src/main/java/cn/anlper/train/MemberApplication8001.java
@@ -0,0 +1,11 @@
+package cn.anlper.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberApplication8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication8001.class);
+    }
+}
