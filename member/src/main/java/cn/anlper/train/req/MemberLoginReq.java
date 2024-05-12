fba455594ed3e9752cfff24e25d5425558d828ce
解矣 <516363082@qq.com>
2024-05-12 23:45:58 +0800

    将参数验证从NotBlank改成NotEmpty，NotBlank只有在null时报错，NotEmpty不仅null报错，在空字符串会报错

--- member/src/main/java/cn/anlper/train/req/MemberLoginReq.java
+++ member/src/main/java/cn/anlper/train/req/MemberLoginReq.java
@@ -1,15 +1,15 @@
 package cn.anlper.train.req;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
 public class MemberLoginReq {
-    @NotBlank(message = "【手机号不能为空】")
+    @NotEmpty(message = "【手机号不能为空】")
     private String mobile;
 
-    @NotBlank(message = "【验证码】不能为空")
+    @NotEmpty(message = "【验证码】不能为空")
     private String code;
 }
--- member/src/main/java/cn/anlper/train/req/MemberRegisterReq.java
+++ member/src/main/java/cn/anlper/train/req/MemberRegisterReq.java
@@ -1,12 +1,12 @@
 package cn.anlper.train.req;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
 public class MemberRegisterReq {
-    @NotBlank(message = "【手机号不能为空】")
+    @NotEmpty(message = "【手机号不能为空】")
     private String mobile;
 }
