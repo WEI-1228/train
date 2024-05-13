4753d4978e2193203a1af993f3d534a01a2d0048
解矣 <516363082@qq.com>
2024-05-13 23:08:25 +0800

    增加乘车人表，生成持久层代码

---
+++ member/src/main/java/cn/anlper/train/mapper/PassengerMapper.java
@@ -0,0 +1,7 @@
+package cn.anlper.train.mapper;
+
+import cn.anlper.train.entities.Passenger;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PassengerMapper extends Mapper<Passenger> {
+}
