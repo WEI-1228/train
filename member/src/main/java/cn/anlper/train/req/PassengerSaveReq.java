139892c97dcd3c2b935f2ebda7d7d40de8fe8a34
解矣 <516363082@qq.com>
2024-05-14 16:14:26 +0800

    增加字段非空校验

--- member/src/main/java/cn/anlper/train/req/PassengerSaveReq.java
+++ member/src/main/java/cn/anlper/train/req/PassengerSaveReq.java
@@ -1,181 +1,31 @@
 package cn.anlper.train.req;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.ToString;
+
 import java.util.Date;
 
 /**
  * 表名：passenger
  * 表注释：乘车人
 */
-@Table(name = "passenger")
+@Data
+@ToString
 public class PassengerSaveReq {
-    /**
-     * id
-     */
-    @Id
-    @GeneratedValue(generator = "JDBC")
+
     private Long id;
-
-    /**
-     * 会员id
-     */
-    @Column(name = "member_id")
+    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
-
-    /**
-     * 姓名
-     */
+    @NotEmpty(message = "【姓名】不能为空")
     private String name;
-
-    /**
-     * 身份证
-     */
-    @Column(name = "id_card")
+    @NotEmpty(message = "【身份证】不能为空")
     private String idCard;
-
-    /**
-     * 旅客类型|枚举[PassengerTypeEnum]
-     */
+    @NotEmpty(message = "【旅客类型】不能为空")
     private String type;
 
-    /**
-     * 新增时间
-     */
-    @Column(name = "create_time")
     private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @Column(name = "update_time")
     private Date updateTime;
-
-    /**
-     * 获取id
-     *
-     * @return id - id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * 设置id
-     *
-     * @param id id
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * 获取会员id
-     *
-     * @return memberId - 会员id
-     */
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    /**
-     * 设置会员id
-     *
-     * @param memberId 会员id
-     */
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    /**
-     * 获取姓名
-     *
-     * @return name - 姓名
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * 设置姓名
-     *
-     * @param name 姓名
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * 获取身份证
-     *
-     * @return idCard - 身份证
-     */
-    public String getIdCard() {
-        return idCard;
-    }
-
-    /**
-     * 设置身份证
-     *
-     * @param idCard 身份证
-     */
-    public void setIdCard(String idCard) {
-        this.idCard = idCard;
-    }
-
-    /**
-     * 获取旅客类型|枚举[PassengerTypeEnum]
-     *
-     * @return type - 旅客类型|枚举[PassengerTypeEnum]
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * 设置旅客类型|枚举[PassengerTypeEnum]
-     *
-     * @param type 旅客类型|枚举[PassengerTypeEnum]
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    /**
-     * 获取新增时间
-     *
-     * @return createTime - 新增时间
-     */
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    /**
-     * 设置新增时间
-     *
-     * @param createTime 新增时间
-     */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    /**
-     * 获取修改时间
-     *
-     * @return updateTime - 修改时间
-     */
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    /**
-     * 设置修改时间
-     *
-     * @param updateTime 修改时间
-     */
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
