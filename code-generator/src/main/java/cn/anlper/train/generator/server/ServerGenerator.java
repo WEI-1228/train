cac86fa617786cae7ea785539ecd7855e97805d9
解矣 <516363082@qq.com>
2024-05-14 22:37:24 +0800

    将mybatis的配置全部提取到properties，方便使用freemarker一键读取配置，生成模版java代码

--- code-generator/src/main/java/cn/anlper/train/generator/server/ServerGenerator.java
+++ code-generator/src/main/java/cn/anlper/train/generator/server/ServerGenerator.java
@@ -1,11 +1,11 @@
 package cn.anlper.train.generator.server;
 
-import cn.anlper.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Properties;
 
 public class ServerGenerator {
     static String toPath = "code-generator/src/main/java/cn/anlper/train/generator/test/";
@@ -15,12 +15,26 @@ public class ServerGenerator {
     }
 
     public static void main(String[] args) throws IOException, TemplateException {
+        Properties properties = new Properties();
 
+        FileInputStream fileInputStream = new FileInputStream(configPath);
+        properties.load(fileInputStream);
+        fileInputStream.close();
 
-        FreemarkerUtil.initConfig("test.ftl");
-        HashMap<String, Object> param = new HashMap<>();
-        param.put("domain", "Test");
-        FreemarkerUtil.generator(toPath + "Test.java", param);
+        // 读取属性值
+        String url = properties.getProperty("jdbc.url");
+        String username = properties.getProperty("jdbc.user");
+        String password = properties.getProperty("jdbc.password");
+
+        // 打印属性值
+        System.out.println("URL: " + url);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
+//        FreemarkerUtil.initConfig("test.ftl");
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath + "Test.java", param);
 
 
     }
