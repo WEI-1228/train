279a5f630e812292029ea4cff581a5c1e3c9ce23
解矣 <516363082@qq.com>
2024-05-14 20:01:13 +0800

    格式化返回日期字段

--- member/src/main/java/cn/anlper/train/resp/PassengerQueryResp.java
+++ member/src/main/java/cn/anlper/train/resp/PassengerQueryResp.java
@@ -1,5 +1,6 @@
 package cn.anlper.train.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.ToString;
 
@@ -12,6 +13,9 @@ public class PassengerQueryResp {
     private String name;
     private String idCard;
     private String type;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 }
