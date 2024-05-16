1b5991b412fd50a2db2645414d2cf3abd3a83697
解矣 <516363082@qq.com>
2024-05-16 08:57:12 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/cn/anlper/train/BatchApplication8003.java
@@ -0,0 +1,11 @@
+package cn.anlper.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication8003 {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication8003.class);
+    }
+}
---
+++ batch/src/main/java/cn/anlper/train/controller/TestController.java
@@ -0,0 +1,17 @@
+package cn.anlper.train.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/batch")
+public class TestController {
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, batch!!!";
+    }
+
+}
