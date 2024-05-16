00db2a37cc942c18c27df3b89dc3ad81bff55b14
解矣 <516363082@qq.com>
2024-05-16 09:24:29 +0800

    集成quartz，和springboot自带的定时任务可以共存

---
+++ batch/src/main/java/cn/anlper/train/config/QuartzConfig.java
@@ -0,0 +1,28 @@
+package cn.anlper.train.config;
+
+import cn.anlper.train.job.TestJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail jobDetail() {
+        return JobBuilder.newJob(TestJob.class)
+                .withIdentity("TestJob", "test")
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger trigger(JobDetail jobDetail) {
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity("trigger", "trigger")
+                .startNow()
+                .withSchedule(CronScheduleBuilder.cronSchedule("*/2 * * * * ?"))
+                .build();
+    }
+}
