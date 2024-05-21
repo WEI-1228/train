3b6e4fa30644a63eee6b519d3ac319a1e31a244b
解矣 <516363082@qq.com>
2024-05-21 20:33:21 +0800

    修复错误

--- business/src/main/java/cn/anlper/train/enums/SeatTypeEnum.java
+++ business/src/main/java/cn/anlper/train/enums/SeatTypeEnum.java
@@ -9,9 +9,9 @@ import java.math.BigDecimal;
 @Getter
 public enum SeatTypeEnum {
     YDZ("1", "一等座", new BigDecimal("0.4")),
-    EDZ("1", "二等座", new BigDecimal("0.3")),
-    RW("1", "一等座", new BigDecimal("0.6")),
-    YW("1", "一等座", new BigDecimal("0.5"));
+    EDZ("2", "二等座", new BigDecimal("0.3")),
+    RW("3", "一等卧", new BigDecimal("0.6")),
+    YW("4", "一等卧", new BigDecimal("0.5"));
     private String code;
     private String desc;
 
