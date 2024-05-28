0f37bc3f1c387adfa9f845dff71d67f6bd8cd8b5
解矣 <516363082@qq.com>
2024-05-28 13:26:21 +0800

    解决RocketMQ可能发生的重复消费问题，将RocketMQ消费者消息处理用try包裹起来，不然发生报错都看不到在哪错了

--- rocketmq/src/main/java/cn/anlper/train/config/MqConfiguration.java
+++ rocketmq/src/main/java/cn/anlper/train/config/MqConfiguration.java
@@ -2,6 +2,7 @@ package cn.anlper.train.config;
 
 import cn.anlper.train.service.ConfirmOrderService;
 import cn.anlper.train.entities.ConfirmOrderDTO;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +14,11 @@ import org.apache.rocketmq.client.apis.consumer.FilterExpression;
 import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
 import org.apache.rocketmq.client.apis.consumer.PushConsumer;
 import org.apache.rocketmq.client.apis.producer.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.nio.ByteBuffer;
 import java.util.Collections;
@@ -36,6 +39,9 @@ public class MqConfiguration {
 
     @Value("${mq.endpoint}")
     private String endpoint;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Bean
     public ClientServiceProvider provider() {
@@ -61,16 +67,30 @@ public class MqConfiguration {
                 .setClientConfiguration(clientConfiguration)
                 .setSubscriptionExpressions(Collections.singletonMap(sellTicketTopic, filterExpression))
                 .setMessageListener(messageView -> {
-                    // 消费消息并返回处理结果。
-                    ByteBuffer buffer = messageView.getBody();
-                    byte[] bytes = new byte[buffer.remaining()];
-                    buffer.get(bytes);
-                    // 将字节数组转换回字符串
-                    String message = new String(bytes);
-                    log.info("收到消息：{}", message);
-                    ConfirmOrderDTO confirmOrderDTO = JSON.parseObject(message, ConfirmOrderDTO.class);
-                    confirmOrderService.doConfirm(confirmOrderDTO);
-                    return ConsumeResult.SUCCESS;
+                    try {
+
+                        // 消费消息并返回处理结果。
+                        ByteBuffer buffer = messageView.getBody();
+                        byte[] bytes = new byte[buffer.remaining()];
+                        buffer.get(bytes);
+                        // 将字节数组转换回字符串
+                        String message = new String(bytes);
+                        log.info("收到消息：{}", message);
+                        ConfirmOrderDTO confirmOrderDTO = JSON.parseObject(message, ConfirmOrderDTO.class);
+                        Long id = confirmOrderDTO.getId();
+                        String exist = redisTemplate.opsForValue().get(String.valueOf(id));
+                        if (StrUtil.isNotBlank(exist)) {
+                            log.info("订单id【{}】，重复消费，不处理", id);
+                            return ConsumeResult.SUCCESS;
+                        } else {
+                            redisTemplate.opsForValue().set(String.valueOf(id), "ok");
+                        }
+                        confirmOrderService.doConfirm(confirmOrderDTO);
+                        return ConsumeResult.SUCCESS;
+                    } catch (Exception e) {
+                        log.error("消息处理发生错误：", e);
+                        throw e;
+                    }
                 })
                 .build();
     }
